776056ee97891678af51d42bb10debe3d114b1a0
shangasinga <singa.shanga@outlook.com>
2025-07-21 11:06:47 +0100

    Added an improved solution that optimizes space complexity

--- RunningSumOf1DArray.java
+++ RunningSumOf1DArray.java
@@ -1,4 +1,8 @@
-class RunningSumOf1DArray {
+// The above is a brute force approach with the below performance
+// Time complexity: O(n);
+// Space Complexity: O(n);
+
+/* class RunningSumOf1DArray {
     public int[] runningSum(int[] nums) {
         int runningSumArray[] = new int[nums.length];
         int sum = 0;
@@ -7,5 +11,18 @@ class RunningSumOf1DArray {
             runningSumArray[i]=sum;
         }
         return runningSumArray;
+    }
+} */
+
+// The above is a brute force approach with the below performance
+// Time complexity: O(n);
+// Space Complexity: O(1);
+
+class RunningSumOf1DArray{
+    public int[] runningSum(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
     }
 }
